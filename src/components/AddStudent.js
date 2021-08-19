448736c257eaedc646cec81524c430a7fa789444
Hongshan <zhou.hongshan636@gmail.com>
2021-08-19 17:15:49 +0200

    add-student form

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,12 +1,73 @@
-function AddStudent(props) {
+import React, { Component } from "react";
+
+class AddStudent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: "",
+      profileImg: "",
+    };
+  }
+
+  handleChange = (event) => {
+    let {name,value } = event.target;
+      this.setState({
+        [name]: value,
+      });
+  }
+
+  handleFormSubmit = (event) => {
+      event.preventDefault();
+      this.props.addTheStudent(this.state)
+      this.setState({
+        name: "",
+        age: "",
+        profileImg: "",
+      });
+  }
+
+  render() {
     return (
-        <section>
-            <h2>Add new Student</h2>
-            <div> 
-                Your form goes here :)
-            </div>
-        </section>
+      <section>
+        <h2>Add new Student</h2>
+        <div>
+          <form onSubmit={this.handleFormSubmit}>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={(e) => this.handleChange(e)}
+              />
+            </label>
+
+            <label>
+              Age:
+              <input
+                type="text"
+                name="age"
+                value={this.state.age}
+                onChange={(e) => this.handleChange(e)}
+              />
+            </label>
+
+            <label>
+              Image File:
+              <input
+                type="text"
+                name="profileImg"
+                value={this.state.profileImg}
+                onChange={(e) => this.handleChange(e)}
+              />
+            </label>
+            <button>Create</button>
+          </form>
+        </div>
+      </section>
     );
+  }
 }
 
 export default AddStudent;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,34 @@
-import studentsArr from '../data/students.json'
-import Student from './Student';
-import AddStudent from './AddStudent';
+import students from "../data/students.json";
+import Student from "./Student";
+import AddStudent from "./AddStudent";
+import { Component } from "react";
 
-function Main(props) {
+class Main extends Component {
+  state = {
+    studentsArr: students,
+  };
+
+  addStudentHandler = (theStudent) => {
+    const studentsCopy = [...this.state.studentsArr];
+    studentsCopy.push(theStudent);
+    this.setState({
+      studentsArr: studentsCopy,
+    });
+  };
+
+  render() {
     return (
-        <>
-            <AddStudent />
-            <div className="student-list">
-                <h2>List of Students</h2>
-                {studentsArr.map(studentObj => {
-                    return <Student {...studentObj} />;
-                })}
-            </div>
-        </>
+      <>
+        <AddStudent addTheStudent={this.addStudentHandler} />
+        <div className="student-list">
+          <h2>List of Students</h2>
+          {this.state.studentsArr.map((studentObj) => {
+            return <Student {...studentObj} />;
+          })}
+        </div>
+      </>
     );
+  }
 }
 
 export default Main;
